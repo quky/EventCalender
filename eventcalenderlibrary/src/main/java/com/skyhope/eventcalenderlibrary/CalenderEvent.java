418356dfbade03d852ddf526ee7b126ccd5edb8d
quky <hidroela@gmail.com>
2024-12-03 10:50:54 -0600

    Update CalenderEvent.java

--- eventcalenderlibrary/src/main/java/com/skyhope/eventcalenderlibrary/CalenderEvent.java
+++ eventcalenderlibrary/src/main/java/com/skyhope/eventcalenderlibrary/CalenderEvent.java
@@ -494,7 +494,7 @@ public class CalenderEvent extends LinearLayout implements View.OnClickListener 
                 day.setLayoutParams(new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT));
-                day.setTextSize((int) 16);
+                day.setTextSize((int) 19);
                 day.setSingleLine();
                 day.setPadding(10, 10, 10, 10);
                 day.setGravity(Gravity.CENTER);
